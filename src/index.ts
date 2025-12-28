7d0bea6910661a6fadab1e6f7557dc15997351dd
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-12-29 02:40:26 +0900

    partykit serverの導入

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
 import { Hono } from 'hono'
+import { partyserverMiddleware } from "hono-party";
+import { Connection, Server, WSMessage } from "partyserver";
 
 const honoApp = new Hono();
 const apiApp = honoApp.basePath('/api')
+const wsApp = honoApp.basePath('/ws')
+
+// Multiple party servers
+export class Chat extends Server {
+  onConnect(connection: Connection) {
+    console.log("Connected", connection.id, "to server", this.name);
+  }
+
+  onMessage(connection: Connection, message: WSMessage) {
+    console.log("Message from", connection.id, ":", message);
+    // Send the message to every other connection
+    this.broadcast(message);
+  }
+}
+
+wsApp.use(
+  "*",
+  partyserverMiddleware({ onError: (error) => console.error(error) })
+);
 
 apiApp.get('/', (c) => {
   return c.text('Hello Hono!')
